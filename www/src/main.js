e01d1cc41046c47ec15a4a49ac342526eaec08f3
mabo.daniel@gmail.com <mabo.daniel@gmail.com>
2023-11-21 23:50:59 -0600

    UI editable stats table & paste from clipboard

--- www/src/main.js
+++ www/src/main.js
@@ -1,43 +1,54 @@
 import Alpine from "../libs/alpine.esm.js";
 //window.Alpine = Alpine;
+Alpine.store('equipo', {
+    equipo: [],
+    headers: []
+  });
 
 import Tesseract from "../libs/tesseract.esm.min.js";
 let tworker;
 
-Alpine.store('equipo', {
-    equipo: []
-  })
+import StatList from "./statList.js";
+let headers = ["Pieza", ...StatList];
+Alpine.store("equipo").headers = headers;
 
 
 function processStat(stat) {
-  console.log(stat);
-  let attr, type, value;
-  attr = stat.replaceAll(" ",'');
+  let type, value;
   
   if(stat.includes('%')) type = '%';
   else type = '+';
   
-  [, value] = attr.split('+');
-  [stat] = stat.split('+');
-  value = parseFloat(value.replaceAll('.','').replace(',', '.').replace('%',''));
+  [, value] = stat.replace(" ","").split('+');
+  stat = stat.split('+')[0].replace('ala','a la').trim();
+  try {
+    value = parseFloat(value.replaceAll('.','').replace(',', '.').replace('%',''));
+  }
+  catch(err) {
+    console.error(stat, value, err);
+  }
   
-  return {attr: stat.replace('ala','a la').trim(), type, value}
+  return {attr:headers.findIndex(h=>h==stat), stat, type, value}
 }
 
 function processTextData(text) {
   let aleatorias = text.split('aleatorias')[1]?.split("\n") || [];
   if (aleatorias.length == 0) return false;
   aleatorias = aleatorias.map(stat => {
-    if(/^([ACDR]|PS)/i.test(stat)) return stat;
+    if(/^([ACDR]|PS)/.test(stat)) return stat;
     let [, ...st] = stat.split(" ");
     return st.join(" ");
   }).filter(e => e && e != "");
   aleatorias.splice(4);
-  aleatorias = aleatorias.map(processStat);
+  aleatorias = aleatorias
+    .map(processStat)
+    .reduce((row,s) => {
+      if(s.attr>0) row[s.attr] = s.value + s.type;
+      return row;
+    }, new Array(headers.length).fill(0));
+  aleatorias[0] = ""
   
-  Alpine.store('equipo').equipo.push({
-    pieza: aleatorias.map(s=>s.attr).join(", ")
-  });
+  Alpine.store('equipo').equipo.push(aleatorias);
 }
 
 function fileToImage(file) {
@@ -58,13 +69,21 @@ function fileToImage(file) {
 const fileInput = document.getElementById("inputOpen");
 fileInput.onchange = processImages;
 
-async function processImages(ev) {
+const pasteInput = document.getElementById("inputOpen2");
+pasteInput.onpaste = ev => {
+  ev.preventDefault();
+  fileInput.files = (ev.clipboardData || window.clipboardData).files;
+  processImages();
+}
+
+async function processImages() {
   const tworker = await Tesseract.createWorker('spa');
   const files = fileInput.files;
   const canvas = document.createElement("canvas"),
     ctx = canvas.getContext("2d");
   let img;
   for (const file of files) {
+    if(file.type.indexOf('image') < 0) continue;
     console.log("procesing image", file.name);
 
     img = await fileToImage(file);
---
+++ www/src/statList.js
@@ -0,0 +1,10 @@
+import Alpine from "../libs/alpine.esm.js";
+
+let StatList = [
+"PS", "Crit", "Tasa Critica",
+"Ataque", "Ataque de escarcha", "Ataque de fuego", "Ataque eléctrico", "Ataque físico",
+"Daño de fuego",
+"Resistencia", "Resist.física", "Resistencia a la Electricidad", "Resistencia a la Escarcha", "Resistencia al Fuego"
+];
+
+export default StatList;
