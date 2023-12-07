f63fa089e403d92791efac7831215bd521f85410
Daniel Martínez Bonilla <mabo.daniel@gmail.com>
2023-12-06 19:34:02 -0600

    Update statList.js
    add missing Daño

--- www/src/statList.js
+++ www/src/statList.js
@@ -6,10 +6,10 @@ let StatList = [
 "Tasa Crítica+", "Tasa Crítica%",
 "Ataque+", "Ataque de escarcha+", "Ataque de fuego+", "Ataque eléctrico+", "Ataque físico+",
 "Ataque%", "Ataque de escarcha%", "Ataque de fuego%", "Ataque eléctrico%", "Ataque físico%",
-"Daño de fuego+",
-"Daño de fuego%",
+"Daño de escarcha+", "Daño de fuego+", "Daño Eléctrico+", "Daño Físico+",
+"Daño de escarcha%", "Daño de fuego%", "Daño Eléctrico%", "Daño Físico%",
 "Resistencia+", "Resist. física+", "Resistencia a la Electricidad+", "Resistencia a la Escarcha+", "Resistencia al Fuego+",
-"Resistencia%", "Resist. física%", "Resistencia a la Electricidad%", "Resistencia a la Escarcha%", "Resistencia al Fuego%"
+"Resistencia%", "Resist. física%", "Resistencia a la Electricidad%", "Resistencia a la Escarcha%", "Resistencia al Fuego%",
 ];
 
 export default StatList;
